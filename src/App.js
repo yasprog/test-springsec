522fb18ac6ce4c68b419d4cac279b149bd4f4b0a
yasprog <yasprog@yandex.ru>
2022-02-21 14:20:30 +0300

    Начало. Первый пуск

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(3) //это массив из 2 элементов:
+  // 1й - это значение, которое мы объявили
+  // 2й - функция, предназначенная для того, чтобы изменять состояние
+
+  function increment () {
+    setLikes(likes + 1)  // мы не изменяем функцию напрямую, мы вызываем функцию, которая для этого предназначена
+    // и туда передаем значение, которое на 1 больше наших лайков
+  }
+  function decrement () {
+    setLikes(likes - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>{likes}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+
+      </div>
   );
 }
 
