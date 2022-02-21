500a9fbbfe9a98bff6d340656739a14f877457eb
yasprog <yasprog@yandex.ru>
2022-02-21 14:24:53 +0300

    Начало. Первый пуск

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React, {useState} from "react";
+import React from "react";
 
 function App() {
-  const [likes, setLikes] = useState(3) //это массив из 2 элементов:
-  // 1й - это значение, которое мы объявили
-  // 2й - функция, предназначенная для того, чтобы изменять состояние
-
-  function increment () {
-    setLikes(likes + 1)  // мы не изменяем функцию напрямую, мы вызываем функцию, которая для этого предназначена
-    // и туда передаем значение, которое на 1 больше наших лайков
-  }
-  function decrement () {
-    setLikes(likes - 1)
-  }
-
-  return (
-      <div className="App">
-        <h1>{likes}</h1>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-
-      </div>
-  );
+    return (
+        <div className="App">
+    <h1>Работает</h1>
+        </div>
+    );
 }
 
 export default App;
+
