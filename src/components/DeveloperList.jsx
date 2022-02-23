bd5627699e9b6e404e1537729cc9da970f12f7c9
yasprog <yasprog@yandex.ru>
2022-02-23 15:23:55 +0300

    Начало. Добавление анимации

--- src/components/DeveloperList.jsx
+++ src/components/DeveloperList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DeveloperItem from "./DeveloperItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 //получаем список девелоперов, заголовок списка, функцию (обратного вызова) удаления
 const DeveloperList = ({developers, title, remove}) => {
@@ -13,10 +14,21 @@ const DeveloperList = ({developers, title, remove}) => {
     return (
         <div>
             <h1 style={{textAlign: "center"}}>{title}</h1>
-            {developers.map((developer, index) =>
-                <DeveloperItem remove={remove} number={index + 1} developer={developer} key={developer.id}/>
-                // remove={remove} передаем эту функцию дальше вниз
-            )}
+            <TransitionGroup>
+                {developers.map((developer, index) =>
+                        <CSSTransition
+                            key={developer.id}
+                            timeout={500}
+                            classNames="developer"
+                        >
+
+                        <DeveloperItem remove={remove} number={index + 1} developer={developer} key={developer.id}/>
+                        {/*// remove={remove} передаем эту функцию дальше вниз.*/}
+                        </CSSTransition>
+                )}
+
+            </TransitionGroup>
+
         </div>
     );
 };
